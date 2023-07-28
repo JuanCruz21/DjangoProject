6475cf877c86b9cb75117ac4472ba5a95973bf70
JuanCruz21 <82663723+JuanCruz21@users.noreply.github.com>
2023-07-28 18:14:16 -0500

    Actualizacion

--- Empresas/models.py
+++ Empresas/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class Empresas(models.Model):
     CodEmp = models.IntegerField()
     NomEmp = models.CharField(max_length=100)
+    NitEmp = models.CharField(max_length=100)
+    DesEmp = models.TextField()
+
+class Cargo(models.Model):
+    CodCar = models.IntegerField()
+    NomCar = models.CharField(max_length=200)
+    Empresas = models.ForeignKey(Empresas, on_delete=models.CASCADE)
+
+class Personal(models.Model):
+    CodPer = models.IntegerField()
+    NomPer = models.CharField(max_length=200)
+    Empresas = models.ForeignKey(Empresas, on_delete=models.CASCADE)
+    Cargo   = models.ForeignKey(Cargo, on_delete=models.CASCADE)
