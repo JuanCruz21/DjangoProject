98cce20fdc0008787a812f47933a88caeb67be9c
JuanCruz21 <82663723+JuanCruz21@users.noreply.github.com>
2023-07-31 18:11:54 -0500

    Cambios

--- Empresas/urls.py
+++ Empresas/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Hello)
+    path('', views.Hello),
+    path('Empresas/', views.Empresa),
+    path('Empresas/<int:CodCar>', views.Cargos)
 ]
--- Empresas/views.py
+++ Empresas/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from .models import Empresas, Cargo,Personal
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 def Hello(Saludo):
     return HttpResponse("hello")
+
+def Empresa(request):
+    empresa = list(Empresas.objects.values())
+    return JsonResponse(empresa, safe=False)
+
+def Cargos(request,CodCar):
+    #cargo = Cargo.objects.get(CodCar=CodCar)
+    cargo = get_object_or_404(Cargo, CodCar= CodCar)
+    return HttpResponse('Cargo: %s' % cargo.NomCar)
